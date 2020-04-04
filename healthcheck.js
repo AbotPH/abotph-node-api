fb25bb97757e053e962fc4a1061c467374c8b770
Patrick Maurice Pingol <pmcpingol@gmail.com>
2020-04-04 22:15:01 +0800

    :sparkles: Added healthcheck.js

---
+++ healthcheck.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const http = require('http');
+
+const { API_PORT } = process.env;
+
+const options = {
+  hostname: '0.0.0.0',
+  port: API_PORT,
+  timeout: 3000,
+  path: '/healthcheck',
+  method: 'GET',
+};
+
+const request = http.request(options, (res) => {
+  if (res.statusCode === 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+
+request.on('error', (err) => {
+  console.error(err.message);
+  process.exit(1);
+});
+
+request.end();
